b2f15006127fe1ebced0e8e02af90969d17d376c
chhorm chhatry <chhormchhatry78@gmail.com>
2024-11-11 09:54:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const modal = document.getElementById("myModal");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.querySelector(".close");
+
+
+openModalBtn.onclick = function () {
+    modal.style.display = "block";
+}
+
+closeModalBtn.onclick = function () {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
